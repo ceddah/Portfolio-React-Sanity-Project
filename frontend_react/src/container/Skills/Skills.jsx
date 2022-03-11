d15eebb8f9ef10969a68da26c08dcdcd396bf52d
ceddadh <cedanik1@gmail.com>
2022-03-11 21:37:06 +0100

    work section

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Skills.scss";
+
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+import AppWrap from "../../wrapper/AppWrap";
+import { urlFor, client } from "../../client";
 
 const Skills = () => {
   return <div>Skills</div>;
